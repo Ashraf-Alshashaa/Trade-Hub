ac6d537310f2ed0f1ade2ce494fcdab63ec030ce
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-05-16 17:44:59 +0200

    Initialized fastapi

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
+from db.database import engine
+from db import models
 
+app = FastAPI()
+
+@app.get('/')
+def index():
+  return {'message': 'Welkome to Trade Hub app'}
+
+models.Base.metadata.create_all(engine)
