ba3492943d86cb18429e0fe9db2e4dcd11cf3079
Ladan <ladan.rb@gmail.com>
2024-05-17 16:07:51 +0200

    Update DateTime, Add ProductDisplay Class.

--- schemas/product.py
+++ schemas/product.py
@@ -19,10 +19,23 @@ class ProductBase(BaseModel):
     description: str
     seller_id: int
     price: float
-    date: DATETIME
+    date: DateTime
     condition: ConditionEnum
     state: StateEnum
 
 #Need to add this to the model since Pydentice cannot handle working with complex types such as Datetime
     class Config:
         arbitrary_types_allowed = True
+
+    class ProductDisplay(BaseModel):
+        name: str
+        image: str
+        description: str
+        price: float
+        date: DateTime
+        condition: ConditionEnum
+        state: StateEnum
+
+        class Config():
+            orm_mode = True
+            arbitrary_types_allowed = True
