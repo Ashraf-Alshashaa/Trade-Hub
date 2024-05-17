23603b295faef0df34f71013fc65fd16d060ba1b
Ladan <ladan.rb@gmail.com>
2024-05-17 19:15:34 +0200

    Testing interaction with database.

--- routers/product_post.py
+++ routers/product_post.py
@@ -2,6 +2,8 @@ from . import *
 
 router = APIRouter(prefix='/product',tags=['product'])
 
+
 @router.post('/add', response_model=ProductDisplay)
-def add_item(request: ProductBase, db: Session= Depends(get_db)):
+def add_item(request: ProductBase, db: Session = Depends(get_db)):
     return db_product.add_item(db,request)
+
