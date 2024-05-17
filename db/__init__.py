8dc62cbba8c792707c816a70cd8ef03b7b05fe3d
Ladan <ladan.rb@gmail.com>
2024-05-17 13:10:14 +0200

    Implement CRUD operations for the product model.

---
+++ db/__init__.py
@@ -0,0 +1,3 @@
+from sqlalchemy.orm import Session
+from schemas.product import ProductBase, StateEnum, ConditionEnum
+from db.models import DbProduct
---
+++ db/db_product.py
@@ -0,0 +1,48 @@
+from . import *
+
+
+def create_user(db: Session, request: ProductBase):
+    new_item = DbProduct(
+                    name=request.name,
+                    image=request.image,
+                    description=request.description,
+                    seller_id=request.seller_id,
+                    price=request.price,
+                    date=request.date,
+                    condition=request.condition,
+                    state=request.state)
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
+
+
+def get_all_items(db: Session):
+    return db.query(DbProduct).all()
+
+
+def get_item(db: Session, id: int):
+    return db.query(DbProduct).filter(DbProduct.id == id).first()
+
+
+def modify_item(db: Session, id: int, request: ProductBase):
+    item = db.query(DbProduct).filter(DbProduct.id == id)
+    item.update({
+                DbProduct.name: request.name,
+                DbProduct.image: request.image,
+                DbProduct.description: request.description,
+                DbProduct.seller_id: request.seller_id,
+                DbProduct.price: request.price,
+                DbProduct.date: request.date,
+                DbProduct.condition: request.condition,
+                DbProduct.state: request.state})
+
+    db.commit()
+    return 'ok'
+
+#will come back to handle exceptions
+def delete_item(db: Session, id: int):
+    item = db.query(DbProduct).filter(DbProduct.id==id).first()
+    db.delete(item)
+    db.commit()
+    return 'ok'
