9038ed9842b6a1322dd6155db60469ad9570928b
Ladan-Rabbi <ladan.rabbi@teampicnic.com>
2024-05-20 12:34:58 +0200

    Modify Item. Put added to Routers product_post.py.

--- routers/product_post.py
+++ routers/product_post.py
@@ -1,9 +1,13 @@
 from . import *
 
-router = APIRouter(prefix='/product',tags=['product'])
+router = APIRouter(prefix='/product', tags=['product'])
 
 
 @router.post('/add', response_model=ProductDisplay)
 def add_item(request: ProductBase, db: Session = Depends(get_db)):
-    return db_product.add_item(db,request)
+    return db_product.add_item(db, request)
 
+
+@router.put('/modify/{id}', response_model=ProductDisplay)
+def modify_item(id: int, request: ProductBase, db: Session = Depends(get_db)):
+    return db_product.modify_item(db, id, request)
