72a1a7a917ef3ffc48af3b36e64b7b4edad132fa
Ladan <ladan.rb@gmail.com>
2024-05-20 18:43:57 +0200

    Renamed from product_post.py.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from db.database import engine
 from db import models
-from routers import product_post
+from routers import product
 
 app = FastAPI()
-app.include_router(product_post.router)
+app.include_router(product.router)
 @app.get('/')
 def index():
   return {'message': 'Welkome to Trade Hub app'}
