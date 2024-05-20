d40d6507f466a71eb99510e71f72fe1236452eb6
Ladan <ladan.rb@gmail.com>
2024-05-20 21:20:18 +0200

    Renamed from product.py to product_routers.py. Cuz of aliases the code would not run.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from db.database import engine
 from db import models
-from routers import product
+from routers import product_routers
 
 app = FastAPI()
-app.include_router(product.router)
+app.include_router(product_routers.router)
 @app.get('/')
 def index():
   return {'message': 'Welkome to Trade Hub app'}
