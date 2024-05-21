38150f1cb7a38e3e1810050bcab758807a9dc84d
Ladan <ladan.rb@gmail.com>
2024-05-21 14:16:28 +0200

    merge the product routers.

--- routers/product_router.py
+++ routers/product_router.py
@@ -1,6 +1,13 @@
 from . import *
+from db import db_product
+from schemas.product import ProductDisplay
 
 router = APIRouter(prefix='/product', tags=['product'])
+
+
+@router.get('/{id}', response_model=ProductDisplay)
+def get_product(id: int, db: Session = Depends(get_db)):
+    return db_product.get_product(db, id)
 
 
 @router.post('/add', response_model=ProductDisplay)
