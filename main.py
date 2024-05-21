3d32e2bbbe7a0cff79e0d6177ba36e7b32a39115
gaidds <ad.gaidamovich@gmail.com>
2024-05-21 14:09:31 +0200

    changed name of router for users to user_router.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from fastapi import FastAPI
 from db.database import engine
 from db import models
 from routers.product import router as product_router
-from routers.users import router as product_users
-from auth import authentication
+from routers import user_router
 from routers import product_routers
+from auth import authentication
 
 app = FastAPI()
 app.include_router(product_router)
 app.include_router(product_routers.router)
 app.include_router(authentication.router)
-app.include_router(product_users)
+app.include_router(user_router.router)
 
 @app.get('/')
 def index():
