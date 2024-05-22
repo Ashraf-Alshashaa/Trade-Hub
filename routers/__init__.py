df6f8319de53880be2ef641137adbf4f90d012a5
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-05-22 13:40:28 +0200

    Fixed product router imports

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter, Depends, Query
 from db import db_product
-from schemas import product
-from schemas.product import ProductDisplay, ProductBase
 from sqlalchemy.orm.session import Session
 from db.database import get_db
