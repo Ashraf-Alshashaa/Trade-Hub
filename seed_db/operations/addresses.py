7372143b305d2eb7f5ff3abe02ee91b0347aaa4e
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-05-23 11:29:05 +0200

    Created insert addresses

---
+++ seed_db/operations/addresses.py
@@ -0,0 +1,13 @@
+from . import *
+
+
+def insert_address(session, address):
+    session.add(DbAddress(
+                street_name=address['street_name'],
+                city=address['city'],
+                country=address['country'],
+                postcode=address['postcode'],
+                house_number=address['house_number'],
+                user_id=address['user_id']
+            ))
+    session.commit()
