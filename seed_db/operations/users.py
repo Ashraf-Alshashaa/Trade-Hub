d59d34209ca8cd47ea22c56748977612d18eb0cb
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-05-23 11:29:36 +0200

    Created insert users

---
+++ seed_db/operations/users.py
@@ -0,0 +1,12 @@
+from . import *
+from passlib.context import CryptContext
+from auth.hash import Hash
+
+
+def insert_user(session, user):
+    session.add(DbUser(
+        username=user['username'],
+        email=user['email'],
+        password=Hash.bcrypt(user['password'])
+    ))
+    session.commit()
