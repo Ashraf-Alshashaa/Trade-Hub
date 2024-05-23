ca6e64f70dac3e2dbefac04d5862c8572560983e
Ladan <ladan.rb@gmail.com>
2024-05-23 16:41:34 +0200

    Address Schema, CRUD methods, Routers added.

---
+++ db/db_user_address.py
@@ -0,0 +1,52 @@
+from . import *
+
+from . import *
+from schemas.user_address import AddressBase
+from db.models import DbAddress
+
+
+def add_address(db: Session, request: AddressBase):
+    new_address = DbAddress(
+                    street_name=request.street,
+                    house_number=request.house_number,
+                    city=request.city,
+                    country=request.country,
+                    postcode=request.postcode,
+                    )
+    db.add(new_address)
+    db.commit()
+    db.refresh(new_address)
+    return new_address
+
+
+def my_addresses(db: Session):
+    return db.query(DbAddress).all()
+
+
+def get_address(db: Session, id: int):
+    return db.query(DbAddress).filter(DbAddress.id == id).first()
+
+
+def modify_address(db: Session, id: int, request: AddressBase):
+    address = db.query(DbAddress).filter(DbAddress.id == id)
+    if not address:
+        raise status.HTTPException(status_code=404, detail="Address not found")
+    address.update({
+                DbAddress.street_name: request.street,
+                DbAddress.house_number: request.house_number,
+                DbAddress.city: request.city,
+                DbAddress.country: request.country,
+                DbAddress.postcode: request.postcode,
+                })
+    db.commit()
+    return address.first()
+
+
+def delete_address(db: Session, id: int):
+    address = db.query(DbAddress).filter(DbAddress.id == id).first()
+    if not address:
+        raise status.HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+    db.delete(address)
+    db.commit()
+    return 'ok'
+
