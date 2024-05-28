22b3c20b80935c5d774b972588c5a623d9ad47fe
gaidds <ad.gaidamovich@gmail.com>
2024-05-28 16:13:34 +0200

    fixed exceptions handling

--- db/db_product.py
+++ db/db_product.py
@@ -28,14 +28,14 @@ def get_all_products(db: Session):
 def get_product(db: Session, id: int):
     item = db.query(DbProduct).filter(DbProduct.id == id).first()
     if not item:
-        raise status.HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     return item
 
 
 def modify_product(db: Session, id: int, request: ProductBase):
     item = db.query(DbProduct).filter(DbProduct.id == id)
     if not item:
-        raise status.HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     item.update({
                 DbProduct.name: request.name,
                 DbProduct.image: request.image,
@@ -51,7 +51,7 @@ def modify_product(db: Session, id: int, request: ProductBase):
 def delete_product(db: Session, id: int):
     item = db.query(DbProduct).filter(DbProduct.id == id).first()
     if not item:
-        raise status.HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Products not found")
     db.delete(item)
     db.commit()
     return 'ok'
@@ -60,21 +60,21 @@ def delete_product(db: Session, id: int):
 def get_products_by_seller_and_state(db: Session, seller_id: int, state: StateEnum):
     item = db.query(DbProduct).filter(DbProduct.seller_id == seller_id, DbProduct.state == state).all()
     if not item:
-        raise status.HTTPException(status_code=404, detail="Products not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Products not found")
     return item
 
 
 def get_products_by_seller(db: Session, seller_id: int):
     item = db.query(DbProduct).filter(DbProduct.seller_id == seller_id).all()
     if not item:
-        raise status.HTTPException(status_code=404, detail="Products not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Products not found")
     return item
 
 
 def get_products_bought_by_user(db: Session, user_id: int):
     item = db.query(DbProduct).filter(DbProduct.buyer_id == user_id).all()
     if not item:
-        raise status.HTTPException(status_code=404, detail="Products not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Products not found")
     return item
 
 
@@ -86,5 +86,5 @@ def get_products_user_is_bidding_on(db: Session, user_id: int):
     product_ids = [bid.product_id for bid in pending_bids]
     item = db.query(DbProduct).filter(DbProduct.id.in_(product_ids)).all()
     if not item:
-        raise status.HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Products not found")
     return item
