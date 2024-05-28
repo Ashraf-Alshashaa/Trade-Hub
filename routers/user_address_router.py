3b41f92056d3a23c6e31040ec92d9c6f9c155258
Ladan <ladan.rb@gmail.com>
2024-05-28 11:46:03 +0200

    Exceptions handling.

--- routers/user_address_router.py
+++ routers/user_address_router.py
@@ -19,7 +19,10 @@ def add_address(request: AddressBase, db: Session = Depends(get_db),
 @router.get('/my-addresses', response_model=List[AddressPrivateDisplay])
 def all_my_addresses(db: Session = Depends(get_db),
                  current_user: UserBase = Depends(get_current_user)):
-    return db_user_address.my_addresses(db, current_user.id)
+    try:
+        return db_user_address.my_addresses(db, current_user.id)
+    except AttributeError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No such address exists!")
 
 
 @router.get('/{id}')
@@ -30,7 +33,7 @@ def get_address_privately(address_id: int, db: Session = Depends(get_db),
         filtered_address = [adr for adr in addresses if adr.id == address_id]
         if filtered_address[0].id == address_id:
             return db_user_address.get_address(db, address_id)
-    except IndexError as e:
+    except IndexError:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to see this address!")
 
 
@@ -38,9 +41,12 @@ def get_address_privately(address_id: int, db: Session = Depends(get_db),
 def my_address(db: Session = Depends(get_db),
                current_user: UserBase = Depends(get_current_user)):
     address = db_user_address.get_default_address(db, current_user.id)
-    if current_user.id != address.user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to see this address!")
-    return db_user_address.get_default_address(db, current_user.id)
+    try:
+        if current_user.id != address.user_id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to see this address!")
+        return db_user_address.get_default_address(db, current_user.id)
+    except AttributeError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No such address exists!")
 
 
 @router.put('/modify/{id}', response_model=AddressPrivateDisplay)
@@ -48,9 +54,12 @@ def modify_address(request: AddressBase, id: int,
                    db: Session = Depends(get_db),
                    current_user: UserBase = Depends(get_current_user)):
     address = db_user_address.get_address(db, id)
-    if current_user.id != address.user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to modify this address!")
-    return db_user_address.modify_address(db, id, request)
+    try:
+        if current_user.id != address.user_id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to modify this address!")
+        return db_user_address.modify_address(db, id, request)
+    except AttributeError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No such address exists!")
 
 
 @router.put('/default/{id}')
@@ -58,9 +67,13 @@ def my_default_address(id: int,
                        db: Session = Depends(get_db),
                        current_user: UserBase = Depends(get_current_user)):
     address = db_user_address.get_address(db, id)
-    if current_user.id != address.user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to modify this address!")
-    return db_user_address.set_default_address(db, id, current_user.id)
+    try:
+        if current_user.id != address.user_id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to modify this address!")
+        return db_user_address.set_default_address(db, id, current_user.id)
+    except AttributeError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No such address exists!")
+
 
 
 @router.delete('/delete/{id}')
@@ -68,6 +81,9 @@ def delete_address(id: int,
                    db: Session = Depends(get_db),
                    current_user: UserBase = Depends(get_current_user)):
     address = db_user_address.get_address(db, id)
-    if current_user.id != address.user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete this address!")
-    return db_user_address.delete_address(db, id)
+    try:
+        if current_user.id != address.user_id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete this address!")
+        return db_user_address.delete_address(db, id)
+    except AttributeError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No such address exists!")
