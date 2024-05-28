12047424dee879c0734e5bc3dcc9d9a6cfc5f63f
Ladan <ladan.rb@gmail.com>
2024-05-28 08:48:44 +0200

    get_product updated in db_product.py _NOT WORKING YET_

--- db/db_product.py
+++ db/db_product.py
@@ -1,6 +1,7 @@
 from . import *
-from schemas.product import ProductBase
-from db.models import DbProduct
+from schemas.product import ProductBase, ProductDisplay
+from db.models import DbProduct, DbAddress
+from db.db_user_address import is_first_address
 
 
 def add_product(db: Session, request: ProductBase):
@@ -25,7 +26,25 @@ def get_all_products(db: Session):
 
 
 def get_product(db: Session, id: int):
-    return db.query(DbProduct).filter(DbProduct.id == id).first()
+    item = db.query(DbProduct).filter(DbProduct.id == id).first()
+    address = db.query(DbAddress).filter(DbAddress.default, item.seller_id == DbAddress.user_id).first()
+    if not item:
+        raise status.HTTPException(status_code=404, detail="Product not found")
+    if is_first_address(db, id):
+        db_address = db.query(DbAddress).filter(DbAddress.default).first()
+        if not db_address:
+            address = None
+    product = ProductDisplay(
+        name=item.name,
+        image=item.image,
+        description=item.description,
+        price=item.price,
+        date=item.date,
+        condition=item.condition,
+        state=item.state,
+        address=address
+    )
+    return product
 
 
 def modify_product(db: Session, id: int, request: ProductBase):
