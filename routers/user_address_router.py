81176623f921094c98025655463bb1dcfd4eb00d
Ladan <ladan.rb@gmail.com>
2024-05-29 13:50:40 +0200

    For user update to work properly, we need a new model which does not contain the address

--- routers/user_address_router.py
+++ routers/user_address_router.py
@@ -9,16 +9,22 @@ router = APIRouter(
 )
 
 
-@router.post('', response_model=AddressPrivateDisplay)
-def add_address(request: AddressBase, db: Session = Depends(get_db),
+@router.post('', response_model=AddressPrivateDisplay, )
+def add_address(request: AddressBase, user_id : int, db: Session = Depends(get_db),
                 current_user: UserBase = Depends(get_current_user)):
+    if user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
     request.user_id = current_user.id
     return db_user_address.add_address(db, request, current_user.id)
 
 
 @router.get('', response_model=List[AddressPrivateDisplay])
-def all_my_addresses(db: Session = Depends(get_db), default: Optional[bool] = None,
-                 current_user: UserBase = Depends(get_current_user)):
+def all_my_addresses(user_id : int,
+                    db: Session = Depends(get_db),
+                    default: Optional[bool] = None,
+                    current_user: UserBase = Depends(get_current_user)):
+    if user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
     try:
         if not default:
             return db_user_address.my_addresses(db, current_user.id)
@@ -28,8 +34,10 @@ def all_my_addresses(db: Session = Depends(get_db), default: Optional[bool] = No
 
 
 @router.get('/{id}', response_model=AddressPrivateDisplay)
-def get_address_privately(address_id: int, db: Session = Depends(get_db),
+def get_address_privately(user_id : int, address_id: int, db: Session = Depends(get_db),
                           current_user: UserBase = Depends(get_current_user)):
+    if user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
     try:
         addresses = db_user_address.my_addresses(db, current_user.id)
         filtered_address = [adr for adr in addresses if adr.id == address_id]
@@ -41,9 +49,12 @@ def get_address_privately(address_id: int, db: Session = Depends(get_db),
 
 @router.put('/{id}', response_model=AddressPrivateDisplay)
 def modify_address(request: AddressBase, id: int,
+                    user_id : int,
                    db: Session = Depends(get_db),
                    default: Optional[bool] = None,
                    current_user: UserBase = Depends(get_current_user)):
+    if user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
     address = db_user_address.get_address(db, id)
     if not address:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such address exists!")
@@ -56,9 +67,12 @@ def modify_address(request: AddressBase, id: int,
 
 @router.delete('/{id}')
 def delete_address(id: int,
+                   user_id : int,
                    db: Session = Depends(get_db),
                    current_user: UserBase = Depends(get_current_user)):
     address = db_user_address.get_address(db, id)
+    if user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
     try:
         if current_user.id != address.user_id:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete this address!")
