6a94e0b9dfb60d9382c09036539eae504357a3a8
Ladan <ladan.rb@gmail.com>
2024-05-31 10:43:46 +0200

    Merging private ID routers.

--- routers/user_address_router.py
+++ routers/user_address_router.py
@@ -25,6 +25,7 @@ def add_address(request: AddressBase, user_id : int, db: Session = Depends(get_d
 
 @router.get('', response_model=List[AddressPrivateDisplay])
 def all_my_addresses(user_id : int,
+                     address_id : Optional[int] = None,
                     db: Session = Depends(get_db),
                     default: Optional[bool] = None,
                     current_user: UserBase = Depends(get_current_user)):
@@ -34,25 +35,19 @@ def all_my_addresses(user_id : int,
     """
     if user_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
+    if address_id:
+        addresses = db_user_address.my_addresses(db, current_user.id)
+        try:
+            filtered_address = [adr for adr in addresses if adr.id == address_id]
+            if filtered_address[0].id == address_id:
+                return db_user_address.get_address(db, address_id)
+        except IndexError:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
     if default:
         return db_user_address.get_default_address(db, current_user.id)
     elif default==False:
         return []
     return db_user_address.my_addresses(db, current_user.id)
-
-
-@router.get('/{id}', response_model=AddressPrivateDisplay)
-def get_address_privately(user_id : int, address_id: int, db: Session = Depends(get_db),
-                          current_user: UserBase = Depends(get_current_user)):
-    if user_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
-    addresses = db_user_address.my_addresses(db, current_user.id)
-    try:
-        filtered_address = [adr for adr in addresses if adr.id == address_id]
-        if filtered_address[0].id == address_id:
-            return db_user_address.get_address(db, address_id)
-    except IndexError:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorised.")
 
 
 @router.put('/{id}', response_model=AddressPrivateDisplay)
