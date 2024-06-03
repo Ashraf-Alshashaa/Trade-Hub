222e9fe9760f7b66d67d4cd9ce27abf181e02b17
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-06-03 03:57:58 +0200

    Created get categories endpoint

--- db/db_category.py
+++ db/db_category.py
@@ -11,3 +11,13 @@ def add_category(db: Session, request: CategoryBase):
     db.commit()
     db.refresh(new_category)
     return new_category
+
+
+def get_category(db: Session, id: int):
+    if id:
+        category = db.query(DbCategory).filter(DbCategory.id == id).first()
+        if not category:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'category with id {id} not found')
+        return [category]
+    
+    return db.query(DbCategory).all()
--- routers/category_router.py
+++ routers/category_router.py
@@ -1,6 +1,7 @@
 from . import *
 from schemas.category import CategoryDisplay, CategoryBase
 from db import db_category
+from typing import List, Optional
 
 
 router = APIRouter(prefix='/categories', tags=['categories'])
@@ -9,3 +10,11 @@ router = APIRouter(prefix='/categories', tags=['categories'])
 @router.post('', response_model=CategoryDisplay)
 def add_category(request: CategoryBase, db: Session = Depends(get_db)):
     return db_category.add_category(db, request)
+
+
+@router.get('', response_model=List[CategoryDisplay])
+def get_categories(
+        category_id: Optional[int] = None,
+        db: Session = Depends(get_db)
+):
+    return db_category.get_category(db, category_id)
