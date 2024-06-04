4ef5d5c71b81dc894fd81c5125e59a1dd4fac424
Ladan <ladan.rb@gmail.com>
2024-06-04 06:18:52 +0200

    Create endpoints for websockets connections and its router.

--- routers/bid_router.py
+++ routers/bid_router.py
@@ -3,11 +3,13 @@ from schemas.bid import BidDisplay, BidBase
 from db.models import DbBid, DbProduct, DbUser
 from db import db_bid
 from typing import List, Optional
-from notifications.notification import NotificationCenter, NotificationType
+from notifications.notification import NotificationCenter, NotificationType, InAppNotification
+from fastapi.websockets import WebSocket, WebSocketDisconnect
 
 
 router = APIRouter(prefix='/bids', tags=['bids'])
 notify = NotificationCenter()
+
 
 @router.post('', response_model=BidDisplay)
 def add_bid(request: BidBase, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
@@ -51,3 +53,28 @@ def get_all_bids(
 @router.get('/{id}', response_model=BidDisplay)
 def get_bid(id: int, db: Session = Depends(get_db)):
     return db_bid.get_bid(db, id)
+
+connections: [WebSocket] = []
+inapp = InAppNotification()
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await inapp.connect(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        inapp.disconnect(websocket)
+
+@router.post("/trigger-event/")
+async def trigger_event(message: str):
+    await notify.notify_user(NotificationType.IN_APP,recipient=1, message=message)
+    return {"message": f"Notification sent{message}"}
+
+#
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await websocket.send_text(f"Message text was: {data}")
--- schemas/users.py
+++ schemas/users.py
@@ -28,7 +28,7 @@ class UserUpdateDisplay(BaseModel):
     email: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 
