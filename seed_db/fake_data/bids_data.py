024b9c65c0e75377946facf872f95f6402c9d9ff
gaidds <ad.gaidamovich@gmail.com>
2024-06-05 13:41:22 +0200

    fic bid table

--- seed_db/fake_data/bids_data.py
+++ seed_db/fake_data/bids_data.py
@@ -11,6 +11,6 @@ def data():
         {'date': datetime(2024, 5, 19, 12, 34, 56, 789012), 'status': schemas.bid.BidStatus.PENDING, 'product_id':1 , 'price': 122.56, 'bidder_id': 7},
         {'date': datetime(2024, 5, 20, 12, 34, 56, 789012), 'status': schemas.bid.BidStatus.PENDING, 'product_id':1 , 'price': 175.34, 'bidder_id': 1},
         {'date': datetime(2024, 5, 21, 12, 34, 56, 789012), 'status': schemas.bid.BidStatus.PENDING, 'product_id':1 , 'price': 166.45, 'bidder_id': 8},
-        {'date': datetime(2024, 5, 22, 12, 34, 56, 789012), 'status': schemas.bid.BidStatus.PENDING, 'product_id':1 , 'price': 180.78, 'bidder_id': 5},
+        {'date': datetime(2024, 5, 22, 12, 34, 56, 789012), 'status': schemas.bid.BidStatus.PENDING, 'product_id':1 , 'price': 180.78, 'bidder_id': 4},
         {'date': datetime(2024, 5, 23, 12, 34, 56, 789012), 'status': schemas.bid.BidStatus.PENDING, 'product_id':1 , 'price': 147.23, 'bidder_id': 10}
     ]
