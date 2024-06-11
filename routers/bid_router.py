f12a3b67f477f3d13b9ce86096914d4a7da4eea0
gaidds <ad.gaidamovich@gmail.com>
2024-06-11 16:41:25 +0200

    fix channge bid (choose buyer)

--- routers/bid_router.py
+++ routers/bid_router.py
@@ -54,3 +54,40 @@ def get_all_bids(
 @router.get('/{id}', response_model=BidDisplay)
 def get_bid(id: int, db: Session = Depends(get_db)):
     return db_bid.get_bid(db, id)
+
+
+@router.put('/{id}', response_model=BidDisplay)
+async def change_bid(
+        id: int,
+        db: Session = Depends(get_db),
+        current_user: UserBase = Depends(get_current_user)
+):
+    """
+        Accept an existing bid .
+
+        - **id**: ID of the bid.
+        - **db**: Database session.
+        - **current_user**: Currently authenticated user.
+        """
+
+    # Retrieve the product from the database
+    bid = db_bid.get_bid(db, id)
+    product_id = bid.product_id
+    seller_id = db.query(DbProduct).filter(DbProduct.id == product_id).first().seller_id
+    product = db.query(DbProduct).filter(DbProduct.id == product_id).first()
+
+    # Check if the current user is the seller
+    if seller_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to modify this bid")
+
+    bidder = db.query(DbUser).filter(DbUser.id == bid.bidder_id).first()
+    other_bidders = db.query(DbUser).join(DbBid, DbUser.id == DbBid.bidder_id).filter(DbBid.product_id == product_id).all()
+    other_bidders = [bidder.id for bidder in other_bidders]
+    print(other_bidders)
+    await notify.in_app.broadcast(recipient=other_bidders, message=f"{product.name} is sold.")
+    try:
+        await notify.notify_user(NotificationType.EMAIL,
+                                 recipient=bidder.email, subject="Congratulations! You won the auction!",
+                                 body=f"Hi {bidder.username}! \n\n Your bid for {product.name} is chosen by the seller!")
+    finally:
+        return db_bid.choose_buyer(db, id)
