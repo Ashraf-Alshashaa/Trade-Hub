e7d77dcc31ae17a08e16742c3367b478c0cce6f0
Ladan <ladan.rb@gmail.com>
2024-06-11 16:55:55 +0200

    Fix email notifications in payments router

--- routers/payments_router.py
+++ routers/payments_router.py
@@ -92,7 +92,7 @@ def initiate_payment(payment_request: PaymentRequest,
 
 
 @router.put("/{id}", response_model=PaymentResponse, summary="Update Payment Status")
-def update_payment_status(id: str,
+async def update_payment_status(id: str,
                                 request: PaymentStatusUpdate,
                                 db: Session = Depends(get_db),
                                 current_user: UserBase = Depends(get_current_user)):
@@ -104,41 +104,38 @@ def update_payment_status(id: str,
            """
 
     updated_payment = update_payment(db, id, request)
+    try:
+        if updated_payment.status == PaymentStatus.completed:
+            selected_item_ids = [item.id for item in updated_payment.items]
+            change_bid_status_to_pending(db, current_user.id, selected_item_ids)
+            recipients = {db.query(DbUser).filter(DbUser.id == item.seller_id).first().email
+                      for item in updated_payment.items}
+            await notify.notify_user(NotificationType.EMAIL,
+                           recipient=current_user.email, subject="Payment " + updated_payment.status,
+                           body=f"The payment has been successful! ")
+            paid_products = {}
+            for seller in recipients:
+            # Perform a single query to get all paid products for the current seller
+                products = db.query(DbProduct).join(DbUser, DbUser.id == DbProduct.seller_id).filter(
+                    DbProduct.id.in_(selected_item_ids), DbUser.email == seller
+                    ).all()
+                paid_products[seller] = [product.name for product in products]
+    
+                seller_product = ','.join(paid_products[seller])
+                await notify.notify_user(NotificationType.EMAIL,
+                               recipient=seller, subject="Your products are sold!",
+                               body=f" You sold {seller_product} ! ")
+        elif updated_payment.status == PaymentStatus.failed:
+            await notify.notify_user(NotificationType.EMAIL,
+                           recipient=current_user.email, subject="Payment " + updated_payment.status,
+                           body=f"The payment has been failed! :( ")
+    finally:
+        db.commit()
 
-    if updated_payment.status == PaymentStatus.completed:
-        selected_item_ids = [item.id for item in updated_payment.items]
-        change_bid_status_to_pending(db, current_user.id, selected_item_ids)
-    #     recipients = {db.query(DbUser).filter(DbUser.id == item.seller_id).first().email
-    #                   for item in updated_payment.items}
-    #     try:
-    #         await notify.notify_user(NotificationType.EMAIL,
-    #                        recipient=current_user.email, subject="Payment " + updated_payment.status,
-    #                        body=f"The payment has been successful! ")
-    #         paid_products = {}
-    #         for seller in recipients:
-    #         # Perform a single query to get all paid products for the current seller
-    #             products = db.query(DbProduct).join(DbUser, DbUser.id == DbProduct.seller_id).filter(
-    #                 DbProduct.id.in_(selected_item_ids), DbUser.email == seller
-    #                 ).all()
-    #             paid_products[seller] = [product.name for product in products]
-    #
-    #             seller_product = ','.join(paid_products[seller])
-    #             await notify.notify_user(NotificationType.EMAIL,
-    #                            recipient=seller, subject="Your products are sold!",
-    #                            body=f" You sold {seller_product} ! ")
-    #     finally:
-    #         pass
-    # elif updated_payment.status == PaymentStatus.failed:
-    #     await notify.notify_user(NotificationType.EMAIL,
-    #                        recipient=current_user.email, subject="Payment " + updated_payment.status,
-    #                        body=f"The payment has been failed! :( ")
-
-    db.commit()
-
-    return PaymentResponse(
-        payment_id=updated_payment.id,
-        status=updated_payment.status,
-        amount=updated_payment.amount,
-        description=updated_payment.description,
-        items=updated_payment.items
-    )
+        return PaymentResponse(
+            payment_id=updated_payment.id,
+            status=updated_payment.status,
+            amount=updated_payment.amount,
+            description=updated_payment.description,
+            items=updated_payment.items
+        )
