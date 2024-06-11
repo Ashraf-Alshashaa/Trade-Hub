b79d605c99be41b631bef99a559ea584accb0c4b
Ladan <ladan.rb@gmail.com>
2024-06-11 07:01:04 +0200

    Testing websocket authentication

--- routers/websocket_router.py
+++ routers/websocket_router.py
@@ -1,25 +1,22 @@
 from . import *
-from fastapi import APIRouter
-from typing import Annotated
+from fastapi import APIRouter, Cookie, WebSocketException
+from typing import Annotated, Union
 
 router = APIRouter(prefix='/ws', tags=['notifications'])
 
 
 
-async def get_cookie_or_token(
-    websocket: WebSocket,
-    session: Annotated[str | None, db] = None,
-    token: Annotated[str | None, Query()] = None,
-):
-    if session is None and token is None:
-        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
-    return session or token
-
-
 @router.websocket("/{user_id}")
-async def websocket_endpoint(user_id: int, websocket: WebSocket, current_user: UserBase = Depends(get_current_user)):
-    await notify.in_app.connect(user_id, websocket)
-    connections[user_id] = websocket
+async def websocket_endpoint(user_id: int,
+                             websocket: WebSocket,
+                             token: Annotated[Session, Depends(get_current_user)]):
+    if token:
+        await notify.in_app.connect(user_id, websocket)
+
+        connections[user_id] = websocket
+    await websocket.send_text(
+            f"Session cookie or query token value is: {token}"
+    )
     try:
         while True:
             await websocket.receive_text()
